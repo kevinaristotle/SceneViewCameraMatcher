fb966c402c8b4fcc7a5db0ebc639a4008b6c7144
kevinaristotle <karistotlev@gmail.com>
2019-08-23 20:15:58 -0700

    first commit

---
+++ Assets/Scripts/MatchToSceneViewCam.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MatchToSceneViewCam : MonoBehaviour
+{
+    [Tooltip("Target transform to match SceneView camera's position and rotation")]
+    public Transform tragetTransform;
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(MatchToSceneViewCam))]
+public class MatchToSceneViewCamEditor : Editor
+{
+    MatchToSceneViewCam source;
+
+    private void OnEnable()
+    {
+        source = (MatchToSceneViewCam)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Match To Scene View Cam"))
+        {
+            source.tragetTransform.position = SceneView.GetAllSceneCameras()[0].transform.position;
+            source.tragetTransform.rotation = SceneView.GetAllSceneCameras()[0].transform.rotation;
+        }
+    }
+}
+#endif
---
+++ README.md
@@ -0,0 +1,9 @@
+SceneViewCameraMatcher
+==========
+
+SceneViewCameraMatcher contains a small utility script for matching a specified transform's position and rotation with that of the SceneView camera.
+
+License
+-------
+
+[MIT](LICENSE.md)
